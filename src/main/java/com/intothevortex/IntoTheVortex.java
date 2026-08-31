35bc18cac24f47fface2a7bd3e3bb7d6597dac89
Deo <alfieharrylansdell@gmail.com>
2026-08-31 21:07:49 +0100

    CreativeTab

--- src/main/java/com/intothevortex/IntoTheVortex.java
+++ src/main/java/com/intothevortex/IntoTheVortex.java
@@ -1,13 +1,14 @@
 package com.intothevortex;
 
+import com.intothevortex.command.IntoTheVortexCommands;
+import com.intothevortex.dimension.TardisDimensionManager;
 import com.intothevortex.entity.ModEntityTypes;
 import com.intothevortex.exterior.ExteriorRegistry;
+import com.intothevortex.interior.InteriorRegistry;
+import com.intothevortex.item.ModCreativeTabs;
 import com.intothevortex.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import com.intothevortex.dimension.TardisDimensionManager;
-import com.intothevortex.interior.InteriorRegistry;
-import com.intothevortex.command.IntoTheVortexCommands;
 
 public final class IntoTheVortex implements ModInitializer {
     public static final String MOD_ID = "intothevortex";
@@ -18,7 +19,11 @@ public final class IntoTheVortex implements ModInitializer {
         ModEntityTypes.initialize();
         InteriorRegistry.initialize();
         IntoTheVortexCommands.initialize();
+
         ModItems.initialize();
+        ModCreativeTabs.initialize();
+
         ServerTickEvents.END_SERVER_TICK.register(TardisDimensionManager::tick);
     }
 }
+
