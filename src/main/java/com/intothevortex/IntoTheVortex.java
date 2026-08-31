38983329d6f195f2f17de71cb69eda3316914d26
encdata <encrypted.datta@gmail.com>
2026-09-01 00:32:54 +0300

    Restore dimensions after player join

--- src/main/java/com/intothevortex/IntoTheVortex.java
+++ src/main/java/com/intothevortex/IntoTheVortex.java
@@ -3,6 +3,10 @@ package com.intothevortex;
 import com.intothevortex.command.IntoTheVortexCommands;
 import com.intothevortex.network.RuntimeDimensionPayload;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import com.intothevortex.mixin.ServerPlayerMixin;
+import com.intothevortex.dimension.TardisDimensionManager;
+import net.minecraft.world.level.portal.TeleportTransition;
 import com.intothevortex.dimension.TardisDimensionManager;
 import com.intothevortex.entity.ModEntityTypes;
 import com.intothevortex.exterior.ExteriorRegistry;
@@ -23,6 +27,16 @@ public final class IntoTheVortex implements ModInitializer {
 
         ModItems.initialize();
         PayloadTypeRegistry.clientboundPlay().register(RuntimeDimensionPayload.TYPE, RuntimeDimensionPayload.CODEC);
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            java.util.UUID tardisId = ServerPlayerMixin.intothevortex$consumePendingDimension(handler.getPlayer().getUUID());
+            if (tardisId == null) return;
+            server.execute(() -> {
+                var level = TardisDimensionManager.ensureLoaded(server, tardisId);
+                if (level != null && handler.getPlayer().level() == server.overworld()) {
+                    handler.getPlayer().teleport(new TeleportTransition(level, handler.getPlayer().position(), handler.getPlayer().getDeltaMovement(), handler.getPlayer().getYRot(), handler.getPlayer().getXRot(), TeleportTransition.DO_NOTHING));
+                }
+            });
+        });
 
         ServerTickEvents.END_SERVER_TICK.register(TardisDimensionManager::tick);
     }
